42e48b20dfc15b58bc0e417cf37f160ccbbda04e
Manisha Sharma <26manisha26@gmail.com>
2020-05-07 17:04:21 +0530

    new commit

--- public/js/app.js
+++ public/js/app.js
@@ -53922,8 +53922,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\shop_pizza-master\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\shop_pizza-master\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\yummy_pizza\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\yummy_pizza\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
