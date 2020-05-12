ab1ab951c9c5650f0fc4755ff4ac80b958c0b69e
Manisha Sharma <26manisha26@gmail.com>
2020-05-12 08:00:10 +0530

    order history5

---
+++ old/resources/js/components/Home.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import Item from './Item';
+import '../../styles/home.css';
+
+class Home extends Component {
+
+    state = {
+		
+    }
+
+    render() {
+        return (
+            <div className='wrapper'>
+                <div className="container">
+					<div className="row">
+						{this.props.pizzas.map((item, idx) => (
+							<Item 
+								item={item} 
+								key={idx} 
+								onAddToCart={this.props.onAddToCart}
+								onRemoveFromCart={this.props.onRemoveFromCart} 
+							/>
+						))}
+					</div>
+				</div>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ old/resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PizzaApp</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"><link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <div class="content">
+                <div id="main" class="title">     
+                </div>
+            </div>
+        </div>
+        <script type='text/javascript' src='js/app.js'></script>
+    </body>
+</html>
